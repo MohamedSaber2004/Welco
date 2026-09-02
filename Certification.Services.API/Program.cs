20366032ca95e0ec009dc5233d71c3d4c9db02b0
Mohamed Saber <mmms77990@gmail.com>
2026-09-02 06:03:06 +0300

    updates

--- Certification.Services.API/Program.cs
+++ Certification.Services.API/Program.cs
@@ -37,6 +37,7 @@ namespace Certification.Services.API
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
 
+
             if (env.IsDevelopment() || env.EnvironmentName == "Test")
             {
                 var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
