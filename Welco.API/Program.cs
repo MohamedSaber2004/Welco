84afe2d1aeeab95a2a0592dde1c4a079d8ee1a56
Mohamed Saber <mmms77990@gmail.com>
2026-09-07 12:44:35 +0300

    savechanges

--- Welco.API/Program.cs
+++ Welco.API/Program.cs
@@ -32,7 +32,6 @@ namespace Welco.API
                 builder.WebHost.UseUrls($"http://*:{port}");
             }
 
-
             builder.Configuration.Sources.Clear();
             builder.Configuration
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
