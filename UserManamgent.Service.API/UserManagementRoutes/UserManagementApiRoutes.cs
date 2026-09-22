656ec8f7841222e65f35cdda4e6bc6f4d0cd8267
Mohamed Saber <mohamedsaberahmed@std.mans.edu.eg>
2026-09-22 16:20:51 +0300

    updates

--- UserManamgent.Service.API/UserManagementRoutes/UserManagementApiRoutes.cs
+++ UserManamgent.Service.API/UserManagementRoutes/UserManagementApiRoutes.cs
@@ -64,6 +64,7 @@ namespace UserManamgent.Service.API.UserManagementRoutes
             public const string Base = UserManagementApiRoutes.Base + "/companies";
             public const string GetAll = "";
             public const string GetMyCompany = "my";
+            public const string GetDirectory = "directory";
             public const string GetById = "{id:guid}";
             public const string GetProducts = "{companyId:guid}/products";
             public const string Create = "";
